a89a1bce8a81e0aff84a7ac25f8d9ed2bc097ab3
Martin Šafránek <martin.safranek@itelligence.cz>
2020-10-05 15:44:52 +0200

    Fix error message

--- src/input_validator/validate_params.py
+++ src/input_validator/validate_params.py
@@ -6,4 +6,4 @@ from src.input_validator.input_validate_reposlug import input_validate_reposlug
 
 def validate_params(config, author, path, ref, force, dry_run, output_format, reposlug):
     if input_validate_reposlug(reposlug) != VALID_INPUT:
-        raise click.BadParameter("Failed to load the configuration!")
+        raise click.BadParameter(f'Reposlug "{reposlug}" is not valid!')
