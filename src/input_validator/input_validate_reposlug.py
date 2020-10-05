82656be4344c1dc3f3b0cd4e98eb38860a1eccaf
Martin Šafránek <martin.safranek@itelligence.cz>
2020-10-05 21:05:20 +0200

    Fix reposlug validation

--- src/input_validator/input_validate_reposlug.py
+++ src/input_validator/input_validate_reposlug.py
@@ -3,7 +3,7 @@ from src.constants import INVALID_INPUT, VALID_INPUT
 
 def input_validate_reposlug(reposlug):
     s = reposlug.split("/")
-    if len(s) != 2 or len(s[0]) == 0 or len(s[1]) == 0:
+    if reposlug.count("/") or len(s) != 2 or len(s[0]) == 0 or len(s[1]) == 0:
         return INVALID_INPUT
 
     return VALID_INPUT
